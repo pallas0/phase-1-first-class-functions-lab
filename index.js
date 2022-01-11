a2977f46d7aa31fb161fa03b6e9ec8d4be402cab
amelia.risner0@gmail.com <arisner@berkeley.edu>
2022-01-11 01:45:07 -0800

    commit tree 7debf96a670564431231362aee6977db74a90635

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = drivers => drivers.slice(0, 2);
+
+const returnLastTwoDrivers = drivers => drivers.slice(drivers.length - 2, drivers.length);
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(num) {
+    function multiplier(fare) {
+        return num*fare;
+    }
+    return multiplier;
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, driverFunction) {
+    return driverFunction(drivers);
+}
